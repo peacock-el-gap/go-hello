183bf07c109820f4ab7fc5e83b0b37d0d73ece7d
Peacock El Gap <peacock.el.gap@gmail.com>
2019-02-17 21:02:00 +0100

    Pierwsza zmiana :-)

--- hello.go
+++ hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/peacock-el-gap/go/stringutils"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Println("hello, world")
+	fmt.Println(stringutils.Reverse("ABCDEFGHIJK"))
+	fmt.Println(stringutils.Reverse("123456"))
+	fmt.Println(stringutils.Reverse(stringutils.Reverse("ABC1234")))
 }
